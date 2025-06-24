451227c02234ede5715585f2bf6c4f205c78ada1
LLE4000 <lejeune@live.be>
2025-06-24 16:28:06 +0200

    Update accueil.py

--- modules/accueil.py
+++ modules/accueil.py
@@ -3,7 +3,7 @@ import streamlit as st
 def show():
     st.markdown("<h1 style='text-align: center;'>Études Structure</h1>", unsafe_allow_html=True)
 
-    base_url = "https://raw.githubusercontent.com/LLE4000/Etudes-structure/main/assets"
+    base_url = https://raw.githubusercontent.com/LLE4000/Etudes-structure/main/assets
 
     def render_section(titre_html, tools):
         st.markdown(titre_html, unsafe_allow_html=True)
@@ -11,19 +11,13 @@ def show():
 
         for i, tool in enumerate(tools):
             with cols[i]:
-                if st.button(
-                    label=f"🖱️",  # invisible, car remplacé par HTML dessous
-                    key=f"btn_{tool['page']}"
-                ):
-                    st.session_state.page = tool['page']
-                    st.rerun()
-
-                # Affichage image + texte en dessous du bouton invisible
                 st.markdown(
                     f"""
-                    <div style="text-align: center; margin-top: -50px;">
-                        <img src="{base_url}/{tool['image']}" style="width: 120px; height: 120px; margin-bottom: 8px;" />
-                        <div style="font-size: 16px; color: black;">{tool['label']}</div>
+                    <div style="text-align: center;">
+                        <a href="/?page={tool['page']}" target="_self" style="text-decoration: none;">
+                            <img src="{base_url}/{tool['image']}" style="width: 120px; height: 120px; margin-bottom: 8px;" />
+                            <div style="font-size: 16px; color: black;">{tool['label']}</div>
+                        </a>
                     </div>
                     """,
                     unsafe_allow_html=True
@@ -33,8 +27,9 @@ def show():
     beton_tools = [
         {"image": "Logo_poutre.png", "label": "Poutre", "page": "Poutre"},
         {"image": "Logo_dalle.png", "label": "Dalle", "page": "Dalle"},
-        {"image": "Logo_age.png", "label": "Age béton", "page": "Age béton"},
+        {"image": "Logo_age.png", "label": "Age beton", "page": "Age beton"},
         {"image": "Logo_poutre.png", "label": "Tableau armatures", "page": "Tableau armatures"},
+        
     ]
     render_section("## 🧱 <span style='color:#FF6F61;'>Béton</span>", beton_tools)
 
@@ -48,3 +43,13 @@ def show():
         {"image": "Logo_poutre.png", "label": "Tableau profilés", "page": "Tableau profilés"},
     ]
     render_section("## 🏗️ <span style='color:#FFA500;'>Acier</span>", acier_tools)
+
+Lucien LEJEUNE
+Ingénieur de projet
+Service stabilité et méthodes
+Avenue Demeylaan 66 – B-1160 Bruxelles-Brussel
+RPM Bruxelles/RPR Brussel TVA/BTW BE 0424.905.926
+M.+32 (0)496 67 66 59
+  
+ 
+
